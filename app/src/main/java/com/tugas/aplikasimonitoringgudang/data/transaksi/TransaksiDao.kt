fa94a0fca7b96a243fb4a434578317ecfd500b49
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-10-14 15:41:05 +0700

    penambahan entitas transaksi, database, tampilan transaksi, tampilan login, tampilan register, tampilan load screen

---
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/data/transaksi/TransaksiDao.kt
@@ -0,0 +1,20 @@
+package com.tugas.aplikasimonitoringgudang.data.transaksi
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+interface TransaksiDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(barang: Transaksi)
+
+    @Update
+    suspend fun update(barang: Transaksi)
+
+    @Delete
+    suspend fun delete(barang: Transaksi)
+
+    @Query("SELECT * FROM transaksi_table ORDER BY id_transaksi ASC")
+    fun getAllTransaksi(): LiveData<List<Transaksi>>
+
+
+}
