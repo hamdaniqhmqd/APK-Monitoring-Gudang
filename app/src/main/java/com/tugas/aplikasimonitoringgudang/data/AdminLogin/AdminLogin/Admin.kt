0267ee05bfee08101fa1accfcf62f30f9822e8ee
mrxnexsus <mrxnexsus@gmail.com>
2024-10-15 19:59:11 +0700

    tes2

---
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/data/AdminLogin/AdminLogin/Admin.kt
@@ -0,0 +1,11 @@
+package com.tugas.aplikasimonitoringgudang.data.user
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val username: String,
+    val password: String
+)
