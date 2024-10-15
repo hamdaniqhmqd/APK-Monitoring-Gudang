41c1646511007a77663a76793d07faec9d5a6cfc
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-10-15 21:59:39 +0700

    penyesuaian

--- app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/LoadScreenActivity.kt
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/LoadScreenActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.tugas.aplikasimonitoringgudang.AwalActivity
 import com.tugas.aplikasimonitoringgudang.databinding.ActivityLoadScreenBinding
 
 class LoadScreenActivity : AppCompatActivity() {
