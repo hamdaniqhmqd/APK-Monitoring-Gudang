68032777a68dff5bb00e85c7b96fc61eed62d13b
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-10-16 10:14:43 +0700

    perbaikan intent dan menu

--- app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/AwalActivity.kt
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/AwalActivity.kt
@@ -7,6 +7,7 @@ import com.tugas.aplikasimonitoringgudang.databinding.ActivityAwalBinding
 import com.google.android.material.button.MaterialButton
 import com.tugas.aplikasimonitoringgudang.R
 import com.tugas.aplikasimonitoringgudang.ui.login.LoginActivity
+import com.tugas.aplikasimonitoringgudang.ui.register.RegisterActivity
 
 class AwalActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAwalBinding
@@ -17,9 +18,13 @@ class AwalActivity : AppCompatActivity() {
         binding = ActivityAwalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val loginButton = findViewById<MaterialButton>(R.id.btnLogin)
-        loginButton.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/register/RegisterActivity.kt
@@ -1,21 +1,27 @@
 package com.tugas.aplikasimonitoringgudang.ui.register
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.tugas.aplikasimonitoringgudang.R
+import com.tugas.aplikasimonitoringgudang.databinding.ActivityRegisterBinding
+import com.tugas.aplikasimonitoringgudang.ui.MainActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            intentMainAct()
         }
+    }
+
+    private fun intentMainAct() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
