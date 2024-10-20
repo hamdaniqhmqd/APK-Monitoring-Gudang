e91f4c49f3a4282de78541850a65e815d4b8e52c
leninovitaa12 <leninovitaa12@gmail.com>
2024-10-20 22:33:09 +0700

    lanjutan barang

--- app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/barang/DetailBarangFragment.kt
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/barang/DetailBarangFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.tugas.aplikasimonitoringgudang.R
+import com.tugas.aplikasimonitoringgudang.data.barang.Barang
+import com.tugas.aplikasimonitoringgudang.databinding.FragmentAddEditBarangBinding
+import com.tugas.aplikasimonitoringgudang.databinding.FragmentDetailBarangBinding
+import com.tugas.aplikasimonitoringgudang.veiwModel.BarangViewModel
 
 class DetailBarangFragment : Fragment() {
-
+    private lateinit var barangViewModel: BarangViewModel
+    private var barangId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,56 @@ class DetailBarangFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_barang, container, false)
+        val binding = FragmentDetailBarangBinding.inflate(inflater, container, false)
+
+        barangViewModel = ViewModelProvider(this).get(BarangViewModel::class.java)
+
+        barangId = arguments?.getInt("barangId")
+        barangId?.let {
+            barangViewModel.getBarangById(it).observe(viewLifecycleOwner) { barang ->
+                binding.tvItemName.text = barang.nama_barang
+                binding.tvItemCategoryIsi.text = barang.kategori_barang
+                binding.tvItemPriceIsi.text = barang.harga_barang.toString()
+                binding.tvItemStockIsi.text = barang.stok_barang.toString()
+                binding.tvItemSizesIsi.text = barang.ukuran_barang
+            }
+        }
+
+        binding.btnEdit.setOnClickListener {
+            barangId?.let { id ->
+                onDetailClick(id)
+            }
+        }
+
+        binding.btnHapus.setOnClickListener {
+            barangId?.let { id ->
+                barangViewModel.delete(Barang(id, "", "", 0, 0, ""))
+            }
+
+            toBarangFragment()
+        }
+
+        return binding.root
+    }
+
+    private fun onDetailClick(idBarang: Int) {
+        // Navigasi ke CreateProductFragment dengan ID produk
+        val bundle = Bundle().apply {
+            putInt("barangId", idBarang ?: 0)
+        }
+        val addEditFragment = AddEditBarangFragment()
+        addEditFragment.arguments = bundle
+
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.FragmentMenu, addEditFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun toBarangFragment() {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.FragmentMenu, BarangFragment())
+            .addToBackStack(null)
+            .commit()
     }
 }
