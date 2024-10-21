8e7f1c20ab0199512ed94a30c9a5e8669cf5a31a
thinkaboutzzu <thinkaboutzuu@gmail.com>
2024-10-22 02:30:32 +0700

    fitur dashboard admin

--- app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/admin/AdminProfileActivity.kt
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/ui/admin/AdminProfileActivity.kt
@@ -1,21 +1,18 @@
 package com.tugas.aplikasimonitoringgudang.ui.admin
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import android.content.SharedPreferences
 import android.widget.Toast
-import android.content.Context
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.tugas.aplikasimonitoringgudang.R
 import com.tugas.aplikasimonitoringgudang.ui.login.LoginActivity
 import com.tugas.aplikasimonitoringgudang.ui.user.UserViewModel
-import android.util.Log
 
 class AdminProfileActivity : AppCompatActivity() {
     private lateinit var viewModel: UserViewModel
@@ -48,6 +45,9 @@ class AdminProfileActivity : AppCompatActivity() {
         val logoutOption = findViewById<LinearLayout>(R.id.logoutOption)
         logoutOption.setOnClickListener {
             // Clear login state
+            val sharedPreferencesLogin = getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE)
+            sharedPreferencesLogin.edit().clear().apply()
+
             // Redirect to LoginActivity
             val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
