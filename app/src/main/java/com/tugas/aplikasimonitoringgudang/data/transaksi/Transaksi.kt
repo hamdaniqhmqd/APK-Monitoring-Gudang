273f9a02b751987a5fe5872c12a329e175818df5
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-12-10 21:33:35 +0700

    penambahan untuk akses data API Transaksi

--- app/src/main/java/com/tugas/aplikasimonitoringgudang/data/transaksi/Transaksi.kt
+++ app/src/main/java/com/tugas/aplikasimonitoringgudang/data/transaksi/Transaksi.kt
@@ -2,26 +2,22 @@ package com.tugas.aplikasimonitoringgudang.data.transaksi
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "transaksi_table", )
+@Entity(tableName = "transaksi_table")
 data class Transaksi(
     @PrimaryKey(autoGenerate = true)
-    val id_transaksi: Int = 0,
-    val barang_id: Int = 0,
-    val barang_nama: String,
-    val kategori_barang: String,
-    val harga_barang: Int,
-    val stok_barang: Int,
-    val ukuran_barang: String,
-    val jumlah_barang: Int,
-    val total_harga_barang: Int,
-    val user_id: Int,
-    val user_nama: String,
-    val supplier_id: Int,
-    val supplier_nama: String,
-    val bulan: String,
-    val tanggal: String,
-    val tanggalAkhir: String = "",
-    val status: Int,
-    val statusAkhir: Int = 0,
+    @SerializedName("id_transaksi") val id_transaksi: Int = 0,
+    @SerializedName("barang_id") val barang_id: Int = 0,
+    @SerializedName("jumlah_barang") val jumlah_barang: Int,
+    @SerializedName("total_harga_barang") val total_harga_barang: Int,
+    @SerializedName("user_id") val user_id: Int,
+    @SerializedName("supplier_id") val supplier_id: Int,
+    @SerializedName("bulan") val bulan: String,
+    @SerializedName("tanggal") val tanggal: String,
+    @SerializedName("tanggalAkhir") val tanggalAkhir: String = "",
+    @SerializedName("status") val status: Int,
+    @SerializedName("statusAkhir") val statusAkhir: Int = 0,
+    @SerializedName("created_at") val created_at: String = "",
+    @SerializedName("updated_at") val updated_at: String = ""
 )
